6ec1edbd96dce63a60b9fb5f0c3e3078a204876d
Pervez Iqbal <pervezfunctor@gmail.com>
2019-01-03 13:01:46 +0530

    few more examples for presentation

--- src/Presentation.tsx
+++ src/Presentation.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
-import { Section, Simple, SimpleTable, SimpleViewer, Title } from './devfractal'
-import { SimpleEditor } from './devfractal'
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
+import React, { useState } from 'react'
+import {
+  Box,
+  Button,
+  Icon,
+  Section,
+  Simple,
+  SimpleEditor,
+  SimpleTable,
+  SimpleViewer,
+  Text,
+  Title,
+} from './devfractal'
+import { delay } from './utils'
 
 // tslint:disable
 
@@ -11,6 +24,18 @@ const todoList = [
   { id: 2, title: 'go for a walk', done: false },
   { id: 3, title: 'read news', done: false },
 ]
+
+const asyncTodoList = async () => {
+  await delay(3000)
+  return Promise.resolve(todoList)
+}
+
+const fetchTodoListUrl = 'http://localhost:3000/todos'
+
+const fetchTodoList = async () => {
+  const response = await axios.get(fetchTodoListUrl)
+  return response.data
+}
 
 export const LoginForm = () => (
   <Simple.Form initialValues={loginValues}>
@@ -26,15 +51,29 @@ export const TodoView = () => (
 export const TodoEditor = () => (
   <SimpleEditor object={{ title: 'hello', done: false, id: 1000 }} />
 )
-
 export const TodoList = () => <SimpleTable values={todoList} />
+
+export const Counter = () => {
+  const [count, setCount] = useState(0)
+  const inc = () => setCount(count + 1)
+  const dec = () => setCount(count - 1)
+
+  return (
+    <Box textAlignment="centered" shadowLess>
+      <Button noControl variant="success">
+        <Icon icon={faPlus} />
+      </Button>
+      <Text textSize="2">{count}</Text>
+      <Button noControl variant="danger">
+        <Icon icon={faMinus} />
+      </Button>
+    </Box>
+  )
+}
 
 export const Presentation = () => (
   <Section>
     <Title textAlignment="centered">Devfractal!</Title>
-    <LoginForm />
-    <TodoEditor />
-    <TodoView />
-    <TodoList />
+    <SimpleTable values={fetchTodoList} />
   </Section>
 )
