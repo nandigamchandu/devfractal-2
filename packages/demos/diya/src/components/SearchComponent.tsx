763d150161856cb486d858ee009336032abf225f
nandigam chandu <nandigamchandu@gmail.com>
2019-08-28 23:16:55 +0530

    add parameters to SearchComponent

--- packages/demos/diya/src/components/SearchComponent.tsx
+++ packages/demos/diya/src/components/SearchComponent.tsx
@@ -1,17 +1,25 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Icon } from 'devfractal-ui-core'
-import { array, TypeOf } from 'io-ts'
+import { array, Mixed, TypeOf } from 'io-ts'
 import React from 'react'
 import Autosuggest from 'react-autosuggest'
 import { http as httpAPI } from 'technoidentity-devfractal'
-import { User } from '../common'
+import { HasProps } from 'technoidentity-utils'
 
-export const SearchComponent: React.FC<{ onClick(value: string): void }> = ({
+export const SearchComponent = <Spec extends Mixed & HasProps>({
+  spec,
+  resource,
+  scearchBy,
   onClick,
+}: {
+  readonly spec: Spec
+  readonly resource: string
+  readonly scearchBy: string
+  onClick(value: string): void
 }) => {
   const [state, setState] = React.useState<{
     value: string
-    suggestions: Array<TypeOf<typeof User>>
+    suggestions: Array<TypeOf<typeof spec>>
   }>({
     value: '',
     suggestions: [],
@@ -24,24 +32,24 @@ export const SearchComponent: React.FC<{ onClick(value: string): void }> = ({
   const getSuggestions = async (value: string) => {
     const inputLength = value.length
     try {
-      const users = await http.get(
+      const suggestions = await http.get(
         {
-          resource: 'users',
-          query: `userName_like=^${value}`,
+          resource,
+          query: `${scearchBy}_like=^${value}`,
         },
-        array(User),
+        array(spec),
       )
-      return inputLength === 0 ? [] : users
+      return inputLength === 0 ? [] : suggestions
     } catch (err) {
       throw new Error(err)
     }
   }
 
-  const getSuggestionValue = (suggestion: TypeOf<typeof User>) =>
-    suggestion.userName
+  const getSuggestionValue = (suggestion: TypeOf<typeof spec>) =>
+    suggestion[scearchBy]
 
-  const renderSuggestion = (suggestion: TypeOf<typeof User>) => (
-    <div>{`${suggestion.userName}`}</div>
+  const renderSuggestion = (suggestion: TypeOf<typeof spec>) => (
+    <div>{`${suggestion[scearchBy]}`}</div>
   )
 
   const onChange = (
@@ -56,18 +64,10 @@ export const SearchComponent: React.FC<{ onClick(value: string): void }> = ({
   }
 
   const inputProps = {
-    placeholder: 'Type a User Name',
+    placeholder: `Type a ${scearchBy}`,
     value: state.value,
     onChange,
   }
-
-  //  const renderInputComponent = (inputProps: any) => (
-  //    <div>
-  //      <Input {...inputProps} />
-
-  //    </div>
-  //  )
-
   return (
     <>
       <Autosuggest
@@ -77,7 +77,6 @@ export const SearchComponent: React.FC<{ onClick(value: string): void }> = ({
         getSuggestionValue={getSuggestionValue}
         renderSuggestion={renderSuggestion}
         inputProps={inputProps}
-        // renderInputComponent={renderInputComponent}
       />
       <button type="button" onClick={() => onClick(state.value)}>
         <Icon icon={faSearch} />
