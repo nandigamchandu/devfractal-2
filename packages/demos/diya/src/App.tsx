6894368da9b7664366838d66b7bb26bcde659356
sravanidande <sravanidnd89@gmail.com>
2019-08-30 12:05:52 +0530

    add client-dispatcher routing

--- packages/demos/diya/src/App.tsx
+++ packages/demos/diya/src/App.tsx
@@ -1,12 +1,19 @@
 import 'bulma/css/bulma.css'
 import React from 'react'
+import { BrowserRouter, Route as Routes } from 'react-router-dom'
 import {
+  All,
   Column,
   Columns,
+  Create,
+  Route,
   Router,
   SimpleRedirect,
+  Title,
 } from 'technoidentity-devfractal'
+import { evAPI, planRouteAPI, tripAPI } from './common'
 import { SideMenu } from './components'
+import { DispatcherSideMenu } from './components/DispatcherSideMenu'
 import {
   BatteryRoutes,
   ClientRoutes,
@@ -20,28 +27,80 @@ import {
   UserRoutes,
   VehicleRoutes,
 } from './pages'
+import { EVSList, PlanRouteList, RaiseRequestForm, TripList } from './views'
+
+// export const App = () => (
+//   <Router variant="browser">
+//     <Columns>
+//       <SideMenu />
+
+//       <Column>
+//         <SimpleRedirect from="/" to="/drivers" />
+
+//         <DriverRoutes />
+//         <BatteryRoutes />
+//         <ClientRoutes />
+//         <VehicleRoutes />
+//         <EmployeeRoutes />
+//         <GeoFenceRoutes />
+//         <UserRoutes />
+//         <EVSRoutes />
+
+//         <InvoiceListRoute />
+//         <PlanRouteMapRoute />
+//         <TripListRoute />
+//       </Column>
+//     </Columns>
+//   </Router>
+// )
+
+// export const App = () => (
+//   <Router variant="browser">
+//     <Columns>
+//       <DispatcherSideMenu />
+//       <Column>
+//         <SimpleRedirect from="/" to="/evsAssigned" />
+//         <PlanRouteMapRoute />
+//         <TripListRoute />
+//         <EVSRoutes />
+//       </Column>
+//     </Columns>
+//   </Router>
+// )
+
+export const EVSListRoute = () => (
+  <All path="/evsAssigned" api={evAPI} list={EVSList} />
+)
+
+export const RaiseRequestRoute = () => (
+  <Create
+    api={evAPI}
+    path="/raiseRequest"
+    form={RaiseRequestForm}
+    redirectTo="/evsAssigned"
+  />
+)
+
+export const PlaneRouteListRoute = () => (
+  <All path="/planRoute" api={planRouteAPI} list={PlanRouteList} />
+)
+
+export const ViewTripListRoute = () => (
+  <All path="/viewTrips" api={tripAPI} list={TripList} />
+)
 
 export const App = () => (
-  <Router variant="browser">
+  <Router>
     <Columns>
-      <SideMenu />
-
+      <DispatcherSideMenu />
       <Column>
-        <SimpleRedirect from="/" to="/drivers" />
-
-        <DriverRoutes />
-        <BatteryRoutes />
-        <ClientRoutes />
-        <VehicleRoutes />
-        <EmployeeRoutes />
-        <GeoFenceRoutes />
-        <UserRoutes />
-        <EVSRoutes />
-
-        <InvoiceListRoute />
-        <PlanRouteMapRoute />
-        <TripListRoute />
+        <SimpleRedirect from="/" to="/evsAssigned" />
+        <EVSListRoute />
+        <RaiseRequestRoute />
+        <PlaneRouteListRoute />
+        <ViewTripListRoute />
       </Column>
     </Columns>
   </Router>
 )
+// export const App = () => <Title>hello</Title>
