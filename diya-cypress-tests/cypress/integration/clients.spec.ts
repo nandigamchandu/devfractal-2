7c56c93dd796a5577641281a16cc3891e206ff9b
anusha <anushayarra628@gmail.com>
2019-09-13 18:51:17 +0530

    Add test cases for client module

---
+++ diya-cypress-tests/cypress/integration/clients.spec.ts
@@ -0,0 +1,149 @@
+describe('Logging In - XHR Web Form', () => {
+  const url = '/clients'
+  context('Clients Module', () => {
+    it('Navigate to clients page home page', () => {
+      cy.visit('/clients')
+    })
+
+    it('Check clients title in clients home page', () => {
+      cy.contains('Clients')
+    })
+
+    it('Get validation for empty text field', () => {
+      cy.getByText(/add client/i).click()
+      cy.get('input[name="clientName"]').click()
+      cy.get('input[name="email"]').click()
+      cy.getByText('this is a required field').should(
+        'contain',
+        'this is a required field',
+      )
+    })
+
+    it('Back to clients page for further testing', () => {
+      cy.go('back')
+    })
+
+    it('Navigate to Add clent page', () => {
+      cy.getByText(/add client/i).click()
+    })
+
+    it('Check Create client title in Add client page', () => {
+      cy.contains('Add Client')
+    })
+
+    it('Add client - Create client Page - Submit button for valid data', () => {
+      cy.get('input[name="clientName"]').type('Amazon')
+      cy.get('select[name="contractType"]').select('Monthly')
+      cy.get('input[name="email"]').type('anusha@g.com')
+      cy.get('input[name="numberOfEVS"]')
+        .clear()
+        .type('3')
+      cy.get('input[name="rateOfEVS"]')
+        .clear()
+        .type('430')
+      cy.get('input[name="assignedEVSHistory"]')
+        .clear()
+        .type('45')
+      cy.getAllByText(/submit/i).click()
+    })
+    it('Validating added client- check whether the added client is reflecting or not in table', () => {
+      cy.getByText(/next/i)
+        .click()
+        .click()
+        .click()
+        .click()
+      cy.contains('Amazon')
+    })
+
+    it('Back to client page', () => {
+      cy.go('back')
+    })
+
+    it('Add client - Create client Page - Reset Functionality', () => {
+      cy.getByText(/add client/i).click()
+      cy.get('input[name="clientName"]').type('Amazon')
+      cy.get('select[name="contractType"]').select('Monthly')
+      cy.get('input[name="email"]').type('anusha@g.com')
+      cy.get('input[name="numberOfEVS"]')
+        .clear()
+        .type('3')
+      cy.get('input[name="rateOfEVS"]')
+        .clear()
+        .type('430')
+      cy.get('input[name="assignedEVSHistory"]')
+        .clear()
+        .type('45')
+      cy.getAllByText(/reset/i).click()
+    })
+
+    it('Back to client page', () => {
+      cy.go('back')
+    })
+
+    it('Edit driver page navigation', () => {
+      cy.getByText(/next/i).click()
+      cy.contains('Amazon')
+      cy.getByText('Amazon')
+        .siblings()
+        .last()
+        .children()
+        .children()
+        .first()
+        .click()
+    })
+
+    it('Check Edit client title in Edit client page', () => {
+      cy.contains('Edit client')
+    })
+
+    it('Edit client page', () => {
+      cy.get('input[name="numberOfEVS"]')
+        .clear()
+        .type('4')
+      cy.get('select[name="contractType"]').select('Monthly')
+      cy.get('input[name="rateOfEVS"]')
+        .clear()
+        .type('431')
+      cy.get('input[name="assignedEVSHistory"]')
+        .clear()
+        .type('46')
+      cy.get('input[name="email"]')
+        .clear()
+        .type('anusha@g.com')
+      cy.getAllByText(/submit/i).click()
+    })
+
+    it('Back to client page', () => {
+      cy.visit(url)
+    })
+
+    it('Edit client page navigation', () => {
+      cy.getByText(/next/i)
+        .click()
+        .click()
+        .click()
+        .click()
+      cy.getByText('Amazon')
+        .siblings()
+        .last()
+        .children()
+        .children()
+        .first()
+        .click()
+      cy.get('input[name="clientName"]').clear()
+      cy.get('select[name="contractType"]').select('Weekly')
+      cy.get('input[name="email"]').clear()
+      cy.get('input[name="numberOfEVS"]').clear()
+      cy.get('input[name="rateOfEVS"]').clear()
+      cy.get('input[name="assignedEVSHistory"]').clear()
+      cy.getAllByText(/submit/i).click()
+    })
+    it('Back to clients home page', () => {
+      cy.visit(url)
+    })
+
+    it('Navigate to Users table', () => {
+      cy.getByText(/^users/i).click()
+    })
+  })
+})
